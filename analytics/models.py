74b906e2d587b7a958f067eeb0c248b4d0044960
Madiyor <abdukhashimov@yandex.ru>
2020-02-06 20:49:04 +0500

    added analytics

--- analytics/models.py
+++ analytics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 
-# Create your models here.
+
+class ObjectViewed(models.Model):
+    user = models.ForeignKey(get_user_model(), blank=True,
+                             null=True, on_delete=models.CASCADE)
+    ip_address = models.CharField(max_length=200, null=True, blank=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "{} was viewed {}".format(self.content_object, self.timestamp)
+
+    class Meta:
+        ordering = ['-timestamp']
+        verbose_name = 'Object viewed'
+        verbose_name_plural = 'Objects viewed'
