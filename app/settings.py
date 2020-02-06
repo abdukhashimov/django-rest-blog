6e71894105e546bf9bfa7a123e11b3e8c1bdc662
Madiyor <abdukhashimov@yandex.ru>
2020-02-06 20:52:09 +0500

    Started serializer

--- app/settings.py
+++ app/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     # custom installed apps
     'post.apps.PostConfig',
     'comment.apps.CommentConfig',
